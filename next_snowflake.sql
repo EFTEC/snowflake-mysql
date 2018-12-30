4e4cd882bcb7bc0269228f9a3cf12bec7b5a9a11
Jorge Castro <jcastromail@yahoo.es>
2018-12-30 12:53:05 -0300

    Update next_snowflake.sql

--- next_snowflake.sql
+++ next_snowflake.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE FUNCTION `next_snowflake`(node integer) RETURNS BIGINT(20)
 BEGIN
-    --EFTEC/snowflake-mysql :
+    -- EFTEC/snowflake-mysql :
     -- based on yejianfei/snowflake-mysql https://github.com/yejianfei/snowflake-mysql/blob/master/LICENSE
     -- IT HAS A PROBLEM OF CONCURRENCY. The limit is up to 4096 transactions every 1/1000th of a second.
     -- So, if you system is managing 4 milion of transaction per second (such as Twitter), then this solution is not for you.
