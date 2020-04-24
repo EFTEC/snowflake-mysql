ea497757bdf5727e3b80082cde6682cf0619f015
Jorge Castro <jcastromail@yahoo.es>
2020-04-24 17:48:05 -0400

    Update snowflake_ddl.sql

--- snowflake_ddl.sql
+++ snowflake_ddl.sql
@@ -6,5 +6,4 @@ CREATE TABLE `sec_snowflake` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 -- insert the firsrt value
 INSERT INTO `sec_snowflake` (`stub`) VALUES ('a');
--- you could also need this one if it fails to generate the function (deterministic error...)
-SET GLOBAL log_bin_trust_function_creators = 1;
+
